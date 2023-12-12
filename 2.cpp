12a0bb8c5924d02466de6d3477f89cedd6fc5d91
Jiachen <146822199+Jyejii@users.noreply.github.com>
2023-12-12 13:01:00 +0100

    Uniones

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+union Dato {
+    int i;
+    float f;
+    char str[20];
+};
+
+int main() {
+    union Dato dato;
+
+    dato.i = 10;
+    printf("dato.i: %d\n", dato.i);
+
+    dato.f = 220.5;
+    printf("dato.f: %.2f\n", dato.f);
+
+    strcpy(dato.str, "C programming");
+    printf("dato.str: %s\n", dato.str);
+
+    return 0;
+}
